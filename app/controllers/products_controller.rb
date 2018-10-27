2b5d9d56a015a9d16291e490786168456798bf2f
badlychadly <chadmontoya21@yahoo.com>
2018-10-26 17:12:15 -0700

    add create action to ProductsController

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,5 +4,23 @@ class ProductsController < ApplicationController
         render json: Product.all
     end
 
+    def create
+        product = Product.new(product_params)
+        if product.save
+            render json: product
+        end
+    end
+
+
+    private
+
+    def find_product
+        @product = Product.find_by(id: params[:id])
+    end
+
+    def product_params
+        params.require(:product).permit(:name, :price, :for_sale, :description, :img_url)
+    end
+
 
 end
