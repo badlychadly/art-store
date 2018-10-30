78c1e0962290dbcf09a110817ae47d231c76146e
badlychadly <chadmontoya21@yahoo.com>
2018-10-29 19:05:08 -0700

    install redux-thunk + redux-react, create products_reducer.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import productsReducer from './reducers/products_reducer'
 
-
+import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 
+
+
+const store = createStore(productsReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+applyMiddleware(thunk))
+
+
+
 ReactDOM.render(
-<App />, 
+    <Provider store={store}>
+        <App />
+    </Provider>, 
 document.getElementById('root')
 );
 
