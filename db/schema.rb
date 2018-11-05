33486bdd49159c69675062f9628046994628d100
badlychadly <chadmontoya21@yahoo.com>
2018-11-05 15:54:14 -0800

    add sessions_controller.rb and /login post route

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_25_194759) do
+ActiveRecord::Schema.define(version: 2018_11_05_210008) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
