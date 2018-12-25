f102a2dec404be8306a952569ce152f9313c49b8
badlychadly <chadmontoya21@yahoo.com>
2018-12-24 17:01:22 -0800

    when updating product insert the produt back into it's original postion in the array

--- client/src/reducers/products_reducer.js
+++ client/src/reducers/products_reducer.js
@@ -13,13 +13,23 @@ export default (state = {
         // debugger;
             return {...state, products: state.products.concat(action.product), newProduct: action.product}
         case "DELETE_PRODUCT":
-            products = state.products.filter(product => product.id !== action.product.id);
-            return {...state, products: products} 
+        products = state.products.filter(product => product.id !== action.product.id);
+        return {...state, products: products} 
         case "UPDATE_PRODUCT":
+        // debugger;
         // products = state.filter(product => product.id !== action.product.id)
-            products = [...state.products.filter(product => product.id !== action.product.id), action.product]
-            
-            return {...state, products: products}
+        let productIndex = state.products.findIndex(product => {
+            return product.id === action.product.id
+        })
+
+        
+        // debugger;
+            // products = state.products.filter(product => product.id !== action.product.id)
+            return {...state,
+                products: [...state.products.slice(0, productIndex), {...action.product}, ...state.products.slice(productIndex + 1) ]
+               
+               }
+            // return {...state, products: products.splice(productIndex, 0, )}
         case "RESET_NEW_PRODUCT":
             return {...state, newProduct: {}}
     
