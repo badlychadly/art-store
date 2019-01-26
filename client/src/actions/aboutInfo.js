c70d1ea36f71bac917f5a32034596012377be0f8
badlychadly <chadmontoya21@yahoo.com>
2019-01-25 17:02:18 -0800

    set up actionCreator for getInfo

--- client/src/actions/aboutInfo.js
+++ client/src/actions/aboutInfo.js
@@ -1,8 +1,9 @@
 
 export const getInfo = () => {
     return dispatch => {
-        return fetch('http://10.0.0.99:3001/about').then(resp => {
-            debugger;
+        return fetch('http://10.0.0.99:3001/about').then(resp => resp.json())
+        .then(about => {
+            dispatch({type: "GET_ABOUT_SUCCESS", about})
         })
     }
 }
